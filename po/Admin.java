2b8d6a4a96874cb56da2c0127770656e216358db
yuanshunjie <40288493+yuanshunjie@users.noreply.github.com>
2018-11-30 22:13:16 +0800

    po
    po

---
+++ po/Admin.java
@@ -0,0 +1,63 @@
+package com.onlineordering.po;
+
+public class Admin {
+	private int admId;
+	private String admName;
+	private String admPwd;
+	private String admPnum;
+	private String admAdr;
+	private int admPostage;
+	private int admTableware;
+	private int admState;
+	public int getAdmId() {
+		return admId;
+	}
+	public void setAdmId(int admId) {
+		this.admId = admId;
+	}
+	public String getAdmName() {
+		return admName;
+	}
+	public void setAdmName(String admName) {
+		this.admName = admName;
+	}
+	public String getAdmPwd() {
+		return admPwd;
+	}
+	public void setAdmPwd(String admPwd) {
+		this.admPwd = admPwd;
+	}
+	public String getAdmPnum() {
+		return admPnum;
+	}
+	public void setAdmPnum(String admPnum) {
+		this.admPnum = admPnum;
+	}
+	public String getAdmAdr() {
+		return admAdr;
+	}
+	public void setAdmAdr(String admAdr) {
+		this.admAdr = admAdr;
+	}
+	public int getAdmPostage() {
+		return admPostage;
+	}
+	public void setAdmPostage(int admPostage) {
+		this.admPostage = admPostage;
+	}
+	public int getAdmTableware() {
+		return admTableware;
+	}
+	public void setAdmTableware(int admTableware) {
+		this.admTableware = admTableware;
+	}
+	public int getAdmState() {
+		return admState;
+	}
+	public void setAdmState(int admState) {
+		this.admState = admState;
+	}
+	
+	
+
+}
